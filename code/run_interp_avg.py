66a6ec7476b23f3ed739857282a32f57ecb9cb80
TheKingInYellow <kody.crowell1598@gmail.com>
2016-05-06 11:49:42 -0300

    Swapped axis back to original format before saving.

--- code/run_interp_avg.py
+++ code/run_interp_avg.py
@@ -87,34 +87,53 @@ if __name__ == '__main__':
         v=np.swapaxes(ncfile.Variables.v,0,2)
         w=np.swapaxes(ncfile.Variables.w,0,2)
 
-        print '\tinitializing empty array...'
-        varE = np.empty(nnode, dtype=object)
+        print '\tinitializing empty arrays...'
+        uvarE = np.empty(nnode, dtype=object)
+        vvarE = np.empty(nnode, dtype=object)
+        wvarE = np.empty(nnode, dtype=object)
 
         print '\taveraging...'
         if 'DG' in dir_name:
             for node in xrange(nnode):
-                varE[node] = np.mean(u[ne_idx_dg[node],:,:], axis=0)
+                uvarE[node] = np.mean(u[ne_idx_dg[node],:,:], axis=0)
+                vvarE[node] = np.mean(v[ne_idx_dg[node],:,:], axis=0)
+                wvarE[node] = np.mean(w[ne_idx_dg[node],:,:], axis=0)
 
         elif 'GP' in dir_name:
             for node in xrange(nnode):
-                varE[node] = np.mean(u[ne_idx_gp[node],:,:], axis=0)
+                uvarE[node] = np.mean(u[ne_idx_gp[node],:,:], axis=0)
+                vvarE[node] = np.mean(v[ne_idx_gp[node],:,:], axis=0)
+                wvarE[node] = np.mean(w[ne_idx_gp[node],:,:], axis=0)
 
         elif 'PP' in dir_name:
             for node in xrange(nnode):
-                varE[node] = np.mean(u[ne_idx_pp[node],:,:], axis=0)
+                uvarE[node] = np.mean(u[ne_idx_pp[node],:,:], axis=0)
+                vvarE[node] = np.mean(v[ne_idx_pp[node],:,:], axis=0)
+                wvarE[node] = np.mean(w[ne_idx_pp[node],:,:], axis=0)
 
-        x = np.squeeze([np.vstack(varE[i]) for i in xrange(nnode)])
+        x = np.squeeze([np.vstack(uvarE[i]) for i in xrange(nnode)])
+        y = np.squeeze([np.vstack(vvarE[i]) for i in xrange(nnode)])
+        z = np.squeeze([np.vstack(wvarE[i]) for i in xrange(nnode)])
+
+        print '\trestoring axes...'
+        x = np.swapaxes(x, 0, 2)
+        y = np.swapaxes(y, 0, 2)
+        z = np.swapaxes(z, 0, 2)
+
+        print '\tsize of mean speed(s): {}'.format(x.shape)
 
         print '\tcreating new subdirectory...'
         sim_name = dir_name[15:29]
         savepath = SAVEDIR + 'bfric_' + bf + '/' + loc + '_' + sim_name
         os.makedirs(savepath)
-        savepath = savepath + loc + '_' + sim_name + '.mat'
+        savepath = savepath + loc + '_' + sim_name
 
         print '\tsaving...'
         # benefit of saving in matlab: you don't need to know original shape
         # try in pickle, numpy.save or netcdf4??
-        sp.io.savemat(savepath, mdict={'out': x}, oned_as='row')
+        sp.io.savemat(savepath + '_u.mat', mdict={'out': x}, oned_as='row')
+        sp.io.savemat(savepath + '_v.mat', mdict={'out': y}, oned_as='row')
+        sp.io.savemat(savepath + '_w.mat', mdict={'out': z}, oned_as='row')
 
         # to load the data:
         #     > matdata = scipy.io.loadmat(savepath)
