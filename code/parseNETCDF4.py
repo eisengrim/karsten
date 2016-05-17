4555ff9e27b4c85fc748cde9603da3345d04665c
TheKingInYellow <kody.crowell1598@gmail.com>
2016-05-17 14:28:02 -0300

    Added timestamps, coordinates, connectivity and interpolated velocity pickles.

--- code/parseNETCDF4.py
+++ code/parseNETCDF4.py
@@ -1,5 +1,10 @@
 #! /usr/bin/python2.7
 
+"""
+Parses netCDF4 data from FVCOM in a similar manner to tawe-telemac-utils.
+
+usage: python netCDF4.py path/to/file.nc path/to/vel/simdate path/to/output
+"""
 
 from __future__ import division
 import numpy as np
@@ -10,12 +15,11 @@ import sys, os
 import os.path as osp
 
 
-
-
 if __name__ == '__main__':
 
-
     filename = sys.argv[1]
+    dir_vels = sys.argv[2]
+    outpath = sys.argv[3]
 
     # loading netcdf file
     if filename.endswith('.nc'):
@@ -32,15 +36,51 @@ if __name__ == '__main__':
     except AttributeError as e:
         # exception due to nc.Dataset type data
         if e == AttributeError: jtime = data.variables['time'][:]
-    # creates matlab time
-    mtime = self.jtime[:] + 678942.0
     print 'full temporal domain loaded...'
-    print 'loading velocities...'
 
-    u = data.variables['u'].data
-    v = data.variables['v'].data
-    w = data.variables['w'].data
+    # deal with time data
+    # converts julian time to seconds elapsed
+    times = (jtime - jtime[0])*8640/0.1
+    nt - len(times)
 
+    print 'loading spatial domain and connectivity...'
+    # deal with nodal coordinates
+    x = data.variables['x'].data
+    y = data.variables['y'].data
+    npoin = len(x)
 
+    # deal with elemental coordinates and connectivity
+    nelem = len(data.variables['xc'].data)
+    ndp = 6
+
+    try:
+        nv = np.transpose(data.variables['nv'].data) - 1
+    except AttributeError:
+        # exception due to nc.Dataset type data
+        nv = np.transpose(data.variables['nv'][:]) - 1
+    # since neighbouring nodes are preserved throughout layers,
+    # triangular prisms may be constructed by copying nv...
+    nv = np.hstack((nv,nv))
+
+    print 'loading velocity files...'
+    # deal with velocities
+    u = np.load(dir_vels+'_u.pkl', mmap_mode = 'r')
+    v = np.load(dir_vels+'_v.pkl', mmap_mode = 'r')
+    w = np.load(dir_vels+'_w.pkl', mmap_mode = 'r')
+
+    nvars = 4
+    var0 = 'Z'
+    var1 = 'U'
+    var2 = 'V'
+    var3 = 'W'
+
+    # question: how to get z coordinate? how to print out velocities?
+    print 'loading z coordinate variable...'
+    h = data.variables['h'].data
+    zeta = data.variables['zeta'].data
+    z = h + zeta
+
+    # save to file
+    print 'saving to file...'
 
 
