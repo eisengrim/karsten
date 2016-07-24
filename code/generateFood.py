d29ed12eb8e961c5ab0726a15a40df2de3a42bb5
TheKingInYellow <kody.crowell1598@gmail.com>
2016-07-24 18:07:35 -0300

    Added ability to dump data in xy format rather than lon/lat

--- code/generateFood.py
+++ code/generateFood.py
@@ -16,18 +16,22 @@ if  __name__ == '__main__':
           + 'generateFood.py -f fvcom -b min_lon,max_lon,min_lat,max_lat ' \
           + '-d depth -o out -r radius -s str')
     parser.add_argument('-f', action='store', default=None, dest='path2fvcom', \
-            help='path to fvcom nc file', type=str)
+            help='path to fvcom nc file', type=str, required=True)
     parser.add_argument('-b', action='store', default=None, dest='box', \
-            help='bounding box of for a region within the grid', \
-            metavar='minlon maxlon minlat maxlat',type=float, nargs=4)
+            help='bounding box of for a region within the grid', required=True,\
+            metavar='L',type=float, nargs=4)
     parser.add_argument('-o', action='store', default=None, dest='outpath', \
-            help='path to outfile basename', type=str)
-    parser.add_argument('-r', action='store', default=None, dest='radius', \
+            help='path to outfile basename', type=str, required=True)
+    parser.add_argument('-r', action='store', default=500, dest='radius', \
             help='radius of constant strength', type=float)
-    parser.add_argument('-s', action='store', default=None, dest='strength', \
+    parser.add_argument('-s', action='store', default=100000, dest='strength', \
             help='strength of food source', type=float)
-    parser.add_argument('-d', action='store', default=None, dest='depth', \
+    parser.add_argument('-d', action='store', default=6.0, dest='depth', \
             help='maximum depth tolerated', type=float)
+    parser.add_argument('-l', action='store_true', dest='use_ll', \
+            help="output data in lon/lat format", default=False)
+    parser.add_argument('-x', action='store_true', dest='use_xy', \
+            help="output data in x/y format", default=True)
 
     args = parser.parse_args()
 
@@ -44,16 +48,30 @@ if  __name__ == '__main__':
 
     lon = model.Grid.lon[idx]
     lat = model.Grid.lat[idx]
+    x = model.Grid.x[idx]
+    y = model.Grid.y[idx]
     rads = np.tile(args.radius, len(lon))
     stren = np.tile(args.strength, len(lon))
 
-    print 'lon, lat of size ' + str(lon.shape[0]) + ' ' + str(lat.shape[0]) + '...'
-    print 'saving lon/lat info...'
+    if args.use_ll:
+        print 'lon, lat of size ' + str(lon.shape[0]) + ' ' + \
+                str(lat.shape[0]) + '...'
+        print 'saving lon/lat info...'
 
-    with open(args.outpath + '_ll.txt', 'wb') as f:
-        f.write(b'{}\t2\n'.format(len(lon)))
-        np.savetxt(f, np.vstack((lat, lon, np.zeros(len(lon)), rads, stren)).T, \
-                fmt='%f,%f,%f\t%f\t%f')
+        with open(args.outpath + '_ll.txt', 'wb') as f:
+            f.write(b'{}\t2\n'.format(len(lon)))
+            np.savetxt(f, np.vstack((lat, lon, np.zeros(len(lon)), \
+                    rads, stren)).T, fmt='%f,%f,%f\t%f\t%f')
+
+    if args.use_xy:
+        print 'x, y of size ' + str(x.shape[0]) + ' ' + \
+                str(y.shape[0]) + '...'
+        print 'saving x/y info...'
+
+        with open(args.outpath + '_xy.txt', 'wb') as f:
+            f.write(b'{}\t1\n'.format(len(x)))
+            np.savetxt(f, np.vstack((x, y, np.zeros(len(x)), \
+                    rads, stren)).T, fmt='%f,%f,%f\t%f\t%f')
 
     # np.savetxt(args.outpath + '_ll.txt', np.vstack((lat, lon)).T, fmt='%f, %f')
     # dat = np.vstack((lat,lon)).T
