3c6d8162b76bd64148f845e83cab9d0e838a0d92
TheKingInYellow <kody.crowell1598@gmail.com>
2016-11-29 22:35:46 -0400

    Changed inputs

--- code/simpleRunPyTracker.py
+++ code/simpleRunPyTracker.py
@@ -5,11 +5,12 @@ import time
 import scipy.io as sio
 import matplotlib.pyplot as plt
 
-loc = 'HH'
-sim = '2011_Oct_22-29_AF_3D'
-filename = '/EcoII/acadia_uni/projects/acadia_force_numerical_model_r20/2011-10-22_2011-10-29/output/acadia_force_3d_0001.nc'
+loc = 'MP'
+sim = '2011_Oct_22-29_3D'
+filename = '/EcoII/acadia_uni/projects/acadia_force_numerical_model_r20/' \
+        + '2011-10-22_2011-10-29/output/acadia_force_3d_0001.nc'
 outpath = '/EcoII/acadia_uni/projects/drifters/pyticle_tracker/' \
-        + 'HH_20111022-29/'+loc+'_'+sim+'_output.nc'
+        + 'MPe_2011_10_22-29_3D/'+loc+'_'+sim+'_output.nc'
 
 inlocs = None
 
@@ -21,11 +22,11 @@ options['outputratio']=2
 options['ncformat']='NETCDF4_CLASSIC'
 options['useLL']=True
 options['randomStart']=True
-options['centre']=(-64.625361, 45.206072)
-options['radius']=500
-options['np'] = 10
+options['centre']=(-64.334379, 45.336039)
+options['radius']=2000
+options['np'] = 2500
 options['layer']=0
-options['gridDim']='3D'
+options['gridDim']='2D'
 
 options['diffusion']=False
 # projection for dngridCSR
@@ -40,6 +41,4 @@ start = time.clock()
 mypy=pyticle(filename, inlocs, outpath, options=options)
 mypy.run()
 print('run in: %f' % (time.clock() - start))
-
-
 
