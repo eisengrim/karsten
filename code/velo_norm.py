7a793b551ec4a8f2c05f959f2ff61f7b0c8e6270
TheKingInYellow <kody.crowell1598@gmail.com>
2017-04-18 14:58:12 -0300

    added velocity norm calculation

---
+++ code/velo_norm.py
@@ -0,0 +1,24 @@
+#! /usr/env/python2.7
+import numexpr as ne
+
+def velo_norm(u, v, w = None):
+    """
+    This method computes a velocity norm given 3 cartesian velocities.
+    """
+    #Check if w if there
+    if w is not None:
+        try:
+            vel = ne.evaluate('sqrt(u**2 + v**2 + w**2)').squeeze()
+        except (MemoryError, ServerError) as e:
+            print 'data too large for machine memory or server'
+            raise
+    else:
+        try:
+            # computing velocity norm
+            vel = ne.evaluate('sqrt(u**2 + v**2)').squeeze()
+        except (MemoryError, ServerError) as e:
+            print 'data too large for machine memory or server'
+            raise
+
+    return vel
+
